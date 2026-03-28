a5140a17c0f219c9c082243ea7f886d25466583b
Dev1 <dev1@gmail.com>
2026-03-28 10:57:05 +0530

    Commiting 3 java files

---
+++ Demo3.java
@@ -0,0 +1,10 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+        
+        int result = a - b;
+        
+        System.out.println("Subtraction of " + a + " and " + b + " is: " + result);
+    }
+}
